24324b3944803e50aa97eadd534d9b8d49777dd7
dikdik <dieka.koes@gmail.com>
2016-05-24 10:24:41 +0700

    Order Check in

--- app/checkinEvent.php
+++ app/checkinEvent.php
@@ -21,7 +21,7 @@ class checkinEvent extends Model
 		return checkinEvent::select(DB::raw('*,profile_visitor.id as id_pengunjung'))->join('profile_visitor','checkin_event.id_visitor','=','profile_visitor.id','right')->where('checkin_event.id_visitor',NULL)->get();
 	}
 	function get_join_visitor_page(){
-		return checkinEvent::join('profile_visitor',$this->table.'.id_visitor','=','profile_visitor.id')->paginate(20);
+		return checkinEvent::join('profile_visitor',$this->table.'.id_visitor','=','profile_visitor.id')->orderBy($this->table.'.id','DESC')->paginate(20);
 	}
 	function get_all(){
 		return checkinEvent::all();
